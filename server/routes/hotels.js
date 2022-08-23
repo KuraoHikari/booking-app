c630a41f284aaf7af5ccb59344c88b393a46119b
KuraoHikari <dewaindra705@gmail.com>
2022-08-23 21:28:19 +0700

    update error pass

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -13,7 +13,39 @@ router.post('/', async (req, res) => {
   }
 });
 //update
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    return res.status(200).json(updatedHotel);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 //delete
+router.delete('/:id', async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    return res.status(200).json('hotel has been deleted');
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 //get
+router.get('/:id', async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    return res.status(200).json(hotel);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 //get all
+router.get('/', async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    return res.status(200).json(hotels);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 export default router;
