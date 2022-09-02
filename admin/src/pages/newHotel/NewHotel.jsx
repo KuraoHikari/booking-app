5d8512628a27ba1ae2ecbec4cfc0a38d19b47f4a
KuraoHikari <dewaindra705@gmail.com>
2022-09-02 14:26:50 +0700

    update

--- admin/src/pages/newHotel/NewHotel.jsx
+++ admin/src/pages/newHotel/NewHotel.jsx
@@ -3,9 +3,49 @@ import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
 import { useState } from 'react';
+import { hotelInputs } from '../../formSource';
+import useFetch from '../../hooks/useFetch';
+import axios from 'axios';
 
-const NewHotel = ({ inputs, title }) => {
-  const [file, setFile] = useState('');
+const NewHotel = ({ inputs }) => {
+  const [files, setFiles] = useState('');
+  const [info, setInfo] = useState({});
+  const [rooms, setRooms] = useState([]);
+  const { data, loading, error } = useFetch(`http://localhost:8800/rooms`);
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+  const handleSelect = (e) => {
+    const value = Array.from(e.target.selectedOptions, (option) => option.value);
+    setRooms(value);
+    // setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+  console.log(files);
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      const list = await Promise.all(
+        Object.values(files).map(async (file) => {
+          const data = new FormData();
+          data.append('file', file);
+          data.append('upload_preset', 'upload');
+
+          const uploadRes = await axios.post('https://api.cloudinary.com/v1_1/dw0nynnmv/image/upload', data);
+          console.log(uploadRes.data);
+
+          const { url } = uploadRes.data;
+          return url;
+        })
+      );
+      const newHotel = {
+        ...info,
+        rooms,
+        photos: list,
+      };
+      await axios.post('http://localhost:8800/hotels', newHotel);
+    } catch (error) {}
+  };
 
   return (
     <div className="new">
@@ -13,11 +53,11 @@ const NewHotel = ({ inputs, title }) => {
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>{title}</h1>
+          <h1>Add new Products</h1>
         </div>
         <div className="bottom">
           <div className="left">
-            <img src={file ? URL.createObjectURL(file) : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'} alt="" />
+            <img src={files ? URL.createObjectURL(files[0]) : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'} alt="" />
           </div>
           <div className="right">
             <form>
@@ -25,16 +65,36 @@ const NewHotel = ({ inputs, title }) => {
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
                 </label>
-                <input type="file" id="file" onChange={(e) => setFile(e.target.files[0])} style={{ display: 'none' }} />
+                <input type="file" multiple id="file" onChange={(e) => setFiles(e.target.files)} style={{ display: 'none' }} />
               </div>
 
-              {inputs.map((input) => (
+              {hotelInputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input id={input.id} onChange={handleChange} type={input.type} placeholder={input.placeholder} />
                 </div>
               ))}
-              <button>Send</button>
+              <div className="formInput">
+                <label>Featured</label>
+                <select id="featured" onChange={handleChange}>
+                  <option value={false}>No</option>
+                  <option value={true}>Yes</option>
+                </select>
+              </div>
+              <div className="selectRooms">
+                <label>Rooms</label>
+                <select id="rooms" multiple onChange={handleSelect}>
+                  {loading
+                    ? 'loading'
+                    : data &&
+                      data.map((room) => (
+                        <option key={room._id} value={room._id}>
+                          {room.title}
+                        </option>
+                      ))}
+                </select>
+              </div>
+              <button onClick={handleClick}>Send</button>
             </form>
           </div>
         </div>
--- admin/src/pages/newRoom/NewRoom.jsx
+++ admin/src/pages/newRoom/NewRoom.jsx
@@ -3,9 +3,26 @@ import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
 import { useState } from 'react';
+import { roomInputs } from '../../formSource';
+import useFetch from '../../hooks/useFetch';
+import axios from 'axios';
 
-const NewRoom = ({ inputs, title }) => {
-  const [file, setFile] = useState('');
+const NewRoom = ({ inputs }) => {
+  const [info, setInfo] = useState({});
+  const [hotelId, setHotelId] = useState(undefined);
+  const [rooms, setRooms] = useState('');
+  const { data, loading, error } = useFetch(`http://localhost:8800/hotels`);
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const roomNumbers = rooms.split(',').map((room) => ({ number: room }));
+    try {
+      await axios.post(`http://localhost:8800/rooms/${hotelId}`, { ...info, roomNumbers });
+    } catch (error) {}
+  };
 
   return (
     <div className="new">
@@ -13,28 +30,38 @@ const NewRoom = ({ inputs, title }) => {
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>{title}</h1>
+          <h1>Add new Room</h1>
         </div>
         <div className="bottom">
-          <div className="left">
+          {/* <div className="left">
             <img src={file ? URL.createObjectURL(file) : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'} alt="" />
-          </div>
+          </div> */}
           <div className="right">
             <form>
-              <div className="formInput">
-                <label htmlFor="file">
-                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
-                </label>
-                <input type="file" id="file" onChange={(e) => setFile(e.target.files[0])} style={{ display: 'none' }} />
-              </div>
-
-              {inputs.map((input) => (
+              {roomInputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input id={input.id} type={input.type} placeholder={input.placeholder} onChange={handleChange} />
                 </div>
               ))}
-              <button>Send</button>
+              <div className="formInput">
+                <label>Rooms</label>
+                <textarea onChange={(e) => setRooms(e.target.value)} placeholder="give comma between room numbers" />
+              </div>
+              <div className="formInput">
+                <label>Choose a Hotel</label>
+                <select id="hotelId" onChange={(e) => setHotelId(e.target.value)}>
+                  {loading
+                    ? 'loading'
+                    : data &&
+                      data.map((hotel) => (
+                        <option value={hotel._id} key={hotel._id}>
+                          {hotel.name}
+                        </option>
+                      ))}
+                </select>
+              </div>
+              <button onClick={handleClick}>Send</button>
             </form>
           </div>
         </div>
